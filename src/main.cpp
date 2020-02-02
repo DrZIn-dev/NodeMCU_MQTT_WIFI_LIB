ea3e7e98f68d2d1d0ebb55bdd2afcbb908f51c45
DrZin <pasawee.dev@gmail.com>
2020-02-02 22:40:34 +0700

    update git ignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,5 @@
 #include <Arduino.h>
-
-#define WIFI_SSID "DrZin"
-#define WIFI_PASS "0985626152a"
-#define CLIENT_ID ""
-#define CLIENT_USERNAME ""
-#define CLIENT_PASSWORD ""
-#define MQTT_SERVER ""
-#define MQTT_PORT 1883
-
-#define PUB_MSG_DATA ""
-#define PUB_SHADOW_DATA ""
+#include "SECRET.hpp"
 
 void setup()
 {
