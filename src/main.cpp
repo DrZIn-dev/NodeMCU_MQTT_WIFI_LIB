c79d689e6d04e747b0cd719d779adfea91b9c255
DrZin <pasawee.dev@gmail.com>
2020-02-02 21:54:44 +0700

    init project

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiClient.h>
+#include <ArduinoJson.h>
+
+
+#define WIFI_SSID "MakeEdu"
+#define WIFI_PASS "Hybrid-6Zero7"
+#define CLIENT_ID ""
+#define CLIENT_USERNAME ""
+#define CLIENT_PASSWORD ""
+#define MQTT_SERVER ""
+#define MQTT_PORT 1883
+
+#define PUB_MSG_DATA ""
+#define PUB_SHADOW_DATA ""
+
+void setup()
+{
+  
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
